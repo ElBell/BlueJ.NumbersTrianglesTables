673987fc90e6a46845c6a9d6df77312f4cf84df8
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-7.local>
2019-02-06 20:37:12 -0500

    Refactored so that getExponentiations() can call getRange()

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -10,14 +10,19 @@ public class NumberUtilities {
     public static String getRange(int start, int stop) {
       return getRange(start, stop, 1);
     }
-
+    
     public static String getRange(int start, int stop, int step) {
+      return getRange(start, stop, step, 1);  
+    }
+
+    public static String getRange(int start, int stop, int step, int expo) {
        StringBuilder range = new StringBuilder(stop-start);
-       for (int i = start; i<stop; i += step) {
-         range.append(i);
+       for (int i = start; i < stop; i += step) {
+         range.append(Math.round(Math.pow(i, expo)));
        }
        return range.toString();
     }
+
 
     public static String getEvenNumbers(int start, int stop) {
        return start%2 != 0 ? getRange(start +1, stop, 2) : getRange(start, stop, 2);
@@ -30,12 +35,6 @@ public class NumberUtilities {
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-
-      StringBuilder expoRange = new StringBuilder(stop-start);
-        for (int i = start; i<=stop; i++) {
-             long expo = Math.round(Math.pow(i, exponent)); 
-             expoRange.append(expo);
-       }
-       return expoRange.toString();
+        return getRange(start, stop + 1, 1, exponent);
      }
 }
